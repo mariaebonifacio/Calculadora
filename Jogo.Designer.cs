b159bd6a56f42c52de1b40a50b56bc9f5ae39a4b
mariaebonifacio <madubonifacio12@gmail.com>
2024-09-17 15:04:24 -0300

    ADD: Design da calculadora

--- Jogo.Designer.cs
+++ Jogo.Designer.cs
@@ -29,19 +29,366 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Jogo));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            button21 = new Button();
+            button23 = new Button();
+            button24 = new Button();
+            button10 = new Button();
+            button25 = new Button();
+            button26 = new Button();
+            textBox1 = new TextBox();
             SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.Desktop;
+            button1.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button1.ForeColor = SystemColors.ControlLightLight;
+            button1.Location = new Point(44, 342);
+            button1.Name = "button1";
+            button1.Size = new Size(56, 55);
+            button1.TabIndex = 0;
+            button1.Text = "1";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.Desktop;
+            button2.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button2.ForeColor = SystemColors.ControlLightLight;
+            button2.Location = new Point(106, 342);
+            button2.Name = "button2";
+            button2.Size = new Size(56, 55);
+            button2.TabIndex = 1;
+            button2.Text = "2";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = SystemColors.Desktop;
+            button3.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button3.ForeColor = SystemColors.ControlLightLight;
+            button3.Location = new Point(168, 342);
+            button3.Name = "button3";
+            button3.Size = new Size(56, 55);
+            button3.TabIndex = 2;
+            button3.Text = "3";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = SystemColors.Desktop;
+            button4.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button4.ForeColor = SystemColors.ControlLightLight;
+            button4.Location = new Point(44, 281);
+            button4.Name = "button4";
+            button4.Size = new Size(56, 55);
+            button4.TabIndex = 3;
+            button4.Text = "4";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = SystemColors.Desktop;
+            button5.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button5.ForeColor = SystemColors.ControlLightLight;
+            button5.Location = new Point(106, 281);
+            button5.Name = "button5";
+            button5.Size = new Size(56, 55);
+            button5.TabIndex = 4;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = SystemColors.Desktop;
+            button6.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button6.ForeColor = SystemColors.ControlLightLight;
+            button6.Location = new Point(168, 281);
+            button6.Name = "button6";
+            button6.Size = new Size(56, 55);
+            button6.TabIndex = 5;
+            button6.Text = "6";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = SystemColors.Desktop;
+            button7.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button7.ForeColor = SystemColors.ControlLightLight;
+            button7.Location = new Point(44, 220);
+            button7.Name = "button7";
+            button7.Size = new Size(56, 55);
+            button7.TabIndex = 6;
+            button7.Text = "7";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = SystemColors.Desktop;
+            button8.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button8.ForeColor = SystemColors.ControlLightLight;
+            button8.Location = new Point(106, 220);
+            button8.Name = "button8";
+            button8.Size = new Size(56, 55);
+            button8.TabIndex = 7;
+            button8.Text = "8";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = SystemColors.Desktop;
+            button9.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button9.ForeColor = SystemColors.ControlLightLight;
+            button9.Location = new Point(168, 220);
+            button9.Name = "button9";
+            button9.Size = new Size(56, 55);
+            button9.TabIndex = 8;
+            button9.Text = "9";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.PaleVioletRed;
+            button11.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button11.Location = new Point(106, 403);
+            button11.Name = "button11";
+            button11.Size = new Size(56, 55);
+            button11.TabIndex = 10;
+            button11.Text = ".";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.PaleVioletRed;
+            button12.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button12.Location = new Point(168, 403);
+            button12.Name = "button12";
+            button12.Size = new Size(56, 55);
+            button12.TabIndex = 11;
+            button12.Text = "=";
+            button12.UseVisualStyleBackColor = false;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.MediumVioletRed;
+            button13.Font = new Font("Arial Narrow", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button13.ForeColor = SystemColors.ControlLightLight;
+            button13.Location = new Point(230, 342);
+            button13.Name = "button13";
+            button13.Size = new Size(56, 116);
+            button13.TabIndex = 12;
+            button13.Text = "+";
+            button13.UseVisualStyleBackColor = false;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.PaleVioletRed;
+            button14.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button14.Location = new Point(230, 281);
+            button14.Name = "button14";
+            button14.Size = new Size(56, 55);
+            button14.TabIndex = 13;
+            button14.Text = "-";
+            button14.UseVisualStyleBackColor = false;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.PaleVioletRed;
+            button15.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button15.Location = new Point(230, 220);
+            button15.Name = "button15";
+            button15.Size = new Size(56, 55);
+            button15.TabIndex = 14;
+            button15.Text = "x";
+            button15.UseVisualStyleBackColor = false;
+            // 
+            // button16
+            // 
+            button16.BackColor = Color.DeepPink;
+            button16.Font = new Font("Arial", 10.8F, FontStyle.Bold);
+            button16.Location = new Point(44, 158);
+            button16.Name = "button16";
+            button16.Size = new Size(56, 55);
+            button16.TabIndex = 15;
+            button16.Text = "AC";
+            button16.UseVisualStyleBackColor = false;
+            // 
+            // button19
+            // 
+            button19.BackColor = Color.PaleVioletRed;
+            button19.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button19.Location = new Point(230, 158);
+            button19.Name = "button19";
+            button19.Size = new Size(56, 55);
+            button19.TabIndex = 18;
+            button19.Text = "÷";
+            button19.UseVisualStyleBackColor = false;
+            // 
+            // button20
+            // 
+            button20.BackColor = Color.PaleVioletRed;
+            button20.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button20.Location = new Point(230, 97);
+            button20.Name = "button20";
+            button20.Size = new Size(56, 55);
+            button20.TabIndex = 22;
+            button20.Text = "½";
+            button20.UseVisualStyleBackColor = false;
+            // 
+            // button21
+            // 
+            button21.BackColor = Color.PaleVioletRed;
+            button21.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button21.Location = new Point(168, 98);
+            button21.Name = "button21";
+            button21.Size = new Size(56, 55);
+            button21.TabIndex = 21;
+            button21.Text = "√";
+            button21.UseVisualStyleBackColor = false;
+            // 
+            // button23
+            // 
+            button23.BackColor = Color.DeepPink;
+            button23.Font = new Font("Arial", 10.8F, FontStyle.Bold);
+            button23.Location = new Point(44, 97);
+            button23.Name = "button23";
+            button23.Size = new Size(56, 55);
+            button23.TabIndex = 19;
+            button23.Text = "OFF";
+            button23.UseVisualStyleBackColor = false;
+            // 
+            // button24
+            // 
+            button24.BackColor = SystemColors.Desktop;
+            button24.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button24.ForeColor = SystemColors.ControlLightLight;
+            button24.Location = new Point(44, 400);
+            button24.Name = "button24";
+            button24.Size = new Size(56, 58);
+            button24.TabIndex = 23;
+            button24.Text = "0";
+            button24.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.DeepPink;
+            button10.Font = new Font("Arial", 10.8F, FontStyle.Bold);
+            button10.Location = new Point(106, 159);
+            button10.Name = "button10";
+            button10.Size = new Size(56, 55);
+            button10.TabIndex = 24;
+            button10.Text = "CE";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button25
+            // 
+            button25.BackColor = Color.DeepPink;
+            button25.Font = new Font("Arial", 10.8F, FontStyle.Bold);
+            button25.Location = new Point(106, 98);
+            button25.Name = "button25";
+            button25.Size = new Size(56, 55);
+            button25.TabIndex = 25;
+            button25.Text = "C";
+            button25.UseVisualStyleBackColor = false;
+            // 
+            // button26
+            // 
+            button26.BackColor = Color.PaleVioletRed;
+            button26.Font = new Font("Arial", 13.8F, FontStyle.Bold);
+            button26.Location = new Point(168, 159);
+            button26.Name = "button26";
+            button26.Size = new Size(56, 55);
+            button26.TabIndex = 26;
+            button26.Text = "%";
+            button26.UseVisualStyleBackColor = false;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(44, 39);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(242, 52);
+            textBox1.TabIndex = 27;
             // 
             // Jogo
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = Color.LightPink;
+            ClientSize = new Size(334, 514);
+            Controls.Add(textBox1);
+            Controls.Add(button26);
+            Controls.Add(button25);
+            Controls.Add(button10);
+            Controls.Add(button24);
+            Controls.Add(button20);
+            Controls.Add(button21);
+            Controls.Add(button23);
+            Controls.Add(button19);
+            Controls.Add(button16);
+            Controls.Add(button15);
+            Controls.Add(button14);
+            Controls.Add(button13);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "Jogo";
             Text = "Jogo";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Button button19;
+        private Button button20;
+        private Button button21;
+        private Button button23;
+        private Button button24;
+        private Button button10;
+        private Button button25;
+        private Button button26;
+        private TextBox textBox1;
     }
 }
