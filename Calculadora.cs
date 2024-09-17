db26fd2edbf47d72da4027f542f3b4cd41ca8781
mariaebonifacio <madubonifacio12@gmail.com>
2024-09-17 11:26:31 -0300

    Add: Programando para abrir outro formulario

--- Calculadora.cs
+++ Calculadora.cs
@@ -6,5 +6,22 @@ namespace Calculadora
         {
             InitializeComponent();
         }
+
+        private void Calculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (progressBar1.Value == 100)
+            {
+                timer1.Enabled = false;
+                Jogo formularioJogo = new Jogo();
+                formularioJogo.ShowDialog();
+            }
+
+            progressBar1.Value = progressBar1.Value + 20;
+        }
     }
 }
